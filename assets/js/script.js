75ebbc52ed73b38ef4b2f231851135250d11eaa8
Kerry Long <victory173451@gmail.com>
2021-12-30 15:43:55 -0500

    adde functionality to load questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var pRemove = $("#pRem");
 var startBtn = $("#start");
 var answersDiv = $("#answers");
+var question = $("#card-header");
 var answerBtn1;
 var answerBtn2;
 var answerBtn3;
@@ -8,8 +9,78 @@ var answerBtn4;
 var tempBtn;
 
 var firstLoad = 1;
+var ndx = 0;
+var score = 0;
 
+var quiz = [
+    {
+        "question": "What is the first index of an array?",
+        "ans1": ["1", "0"],
+        "ans2": ["array length", "0"],
+        "ans3": ["0", "1"],
+        "ans4": ["A", "0"]
+    },
+    {
+        "question": "Which of the following is correct?",
+        "ans1": ["jQuery is a javaScript Library", "1"],
+        "ans2": ["jQuery is a JSON library", "0"],
+        "ans3": ["jQuery is an Angular library", "0"],
+        "ans4": ["All the answers are true", "0"]
+    },
+    {
+        "question": "Which of the following methods is used to hide selected items?",
+        "ans1": ["visible(false)", "0"],
+        "ans2": ["hidden()", "0"],
+        "ans3": ["display(none)", "0"],
+        "ans4": ["hide()", "1"]
+    },
+    {
+        "question": "Which of the following replaces $(document).ready(f)?",
+        "ans1": ["jQuery(f)", "0"],
+        "ans2": ["$(f)", "1"],
+        "ans3": ["#(f)", "0"],
+        "ans4": ["None of the above", "0"]
+    },
+    {
+        "question": "What are jQuery's features?",
+        "ans1": ["Useful collection of methods for manipulating selected items.", "0"],
+        "ans2": ["Efficient query method to find all document elements", "0"],
+        "ans3": ["Syntax for referencing document elements", "0"],
+        "ans4": ["All the answers are true", "1"]
+    }
+];
 
+function loadQuestion(event) {
+
+  
+    if (!(event == 1)) {
+        console.log("data-anser is " + event.target.getAttribute("data-answer"));
+        if ((event.target.getAttribute("data-answer")) == 1) {
+            score++;
+            console.log("score is " + score);
+        }
+    }
+
+    
+
+    
+    console.log((event));
+    if (ndx <= (quiz.length - 1)) {
+        /*answer1.attr("data-answer", quiz[ndx].ans1[1]);
+        answer2.attr("data-answer", quiz[ndx].ans2[1]);
+        answer3.attr("data-answer", quiz[ndx].ans3[1]);
+        answer4.attr("data-answer", quiz[ndx].ans4[1]);*/
+
+        question.text(quiz[ndx].question);
+
+        answerBtn1.html(quiz[ndx].ans1[0]).attr("data-answer", quiz[ndx].ans1[1]);
+        answerBtn2.html(quiz[ndx].ans2[0]).attr("data-answer", quiz[ndx].ans2[1]);
+        answerBtn3.html(quiz[ndx].ans3[0]).attr("data-answer", quiz[ndx].ans3[1]);
+        answerBtn4.html(quiz[ndx].ans4[0]).attr("data-answer", quiz[ndx].ans4[1]);
+
+        ndx++;
+    }
+}
 
 function questionPrep() {
     pRemove.remove();
@@ -35,12 +106,14 @@ function questionPrep() {
     answersDiv.append(answerBtn4);
 
 
-    answerBtn1.on("click", loadQuestions);
-    answerBtn2.on("click", loadQuestions);
-    answerBtn3.on("click", loadQuestions);
-    answerBtn4.on("click", loadQuestions);
+    answerBtn1.on("click", loadQuestion);
+    answerBtn2.on("click", loadQuestion);
+    answerBtn3.on("click", loadQuestion);
+    answerBtn4.on("click", loadQuestion);
    
+    loadQuestion(firstLoad);
   
+    
 }
 tempBtn = startBtn.clone();
 startBtn.on("click", questionPrep);
